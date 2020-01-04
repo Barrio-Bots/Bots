96eeef7fc338bcce36c1f2dd01b310ada410d18a
Jeremy Cantu <mail@jeremycantu.com>
2020-01-04 16:06:04 -0600

    __init__.py added, additional configuration and doc updates

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Adafruit Display_Notification Library'
-copyright = u'2019 Scott Shawcroft'
-author = u'Scott Shawcroft'
+project = u'Bots'
+copyright = u'2019 Jeremy Cantu'
+author = u'Jeremy Cantu'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -109,7 +109,7 @@ html_static_path = ['_static']
 html_favicon = '_static/favicon.ico'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'AdafruitDisplay_notificationLibrarydoc'
+htmlhelp_basename = 'Bots'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -135,7 +135,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'AdafruitDisplay_NotificationLibrary.tex', u'AdafruitDisplay_Notification Library Documentation',
+    (master_doc, 'Bots.tex', u'Bots Library Documentation',
      author, 'manual'),
 ]
 
@@ -144,7 +144,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'AdafruitDisplay_Notificationlibrary', u'Adafruit Display_Notification Library Documentation',
+    (master_doc, 'Botslibrary', u'Bots Library Documentation',
      [author], 1)
 ]
 
@@ -154,7 +154,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'AdafruitDisplay_NotificationLibrary', u'Adafruit Display_Notification Library Documentation',
-     author, 'AdafruitDisplay_NotificationLibrary', 'One line description of project.',
+    (master_doc, 'BotsLibrary', u'Bots Library Documentation',
+     author, 'BotsLibrary', 'One line description of project.',
      'Miscellaneous'),
 ]

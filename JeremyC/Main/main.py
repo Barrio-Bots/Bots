e464e1b5e62fde2ccee7b9424cd58e3d097c52c1
Jeremy Cantu <mail@jeremycantu.com>
2020-01-12 22:44:15 -0600

    Added actual bot code and configuration files

--- JeremyC/Main/main.py
+++ JeremyC/Main/main.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python
-"""Main bot module"""
+from digitalio import DigitalInOut, Pull, Direction
+from adafruit_crickit import crickit
+import board
 
-print("Hello, Bot!")
+# Two onboard CPX buttons for input (low level saves memory)
+button_a = DigitalInOut(board.BUTTON_A)
+button_a.direction = Direction.INPUT
+button_a.pull = Pull.DOWN
+
+button_b = DigitalInOut(board.BUTTON_B)
+button_b.direction = Direction.INPUT
+button_b.pull = Pull.DOWN
+
+# Create one motor on seesaw motor port #1
+motor = crickit.dc_motor_1
+
+
+def start_motor():
+    print("Button A pressed, go!")
+    motor.throttle = 1.0  # full speed!
+
+
+def stop_motor():
+    print("Button B pressed, stop!")
+    motor.throttle = 0    # stop!
+
+
+while True:
+    if button_a.value:
+    	start_motor()
+
+    if button_b.value:
+    	stop_motor()
